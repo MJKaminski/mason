0663315bd4b19ee59053dce5327d804521c31710
SigmaX <fyedernoggersnodden@gmail.com>
2018-12-05 17:09:05 -0500

    Remove mason's SETUP.sh, because it is no longer necessary to manually install dependencies in the local repository.  Also fix the symlink creation line of the root SETUP.sh so that it creats a symlink even if one is already present (a user reported that they were seeing a symlink to /home/hatfolk/... even after running SETUP.sh).

--- SETUP.sh
+++ SETUP.sh
@@ -1,9 +1,8 @@
 cd mason
-./SETUP.sh
 mvn package
 mvn install
 cd ..
-ln -s $(pwd)/mason/target/mason*.jar $(pwd)/contrib/geomason/local-repo/mason.jar
+ln -sf $(pwd)/mason/target/mason*.jar $(pwd)/contrib/geomason/local-repo/mason.jar
 cd contrib/geomason
 ./SETUP.sh
 mvn package
